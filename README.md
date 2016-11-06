dfaf49b2f18235b92892c5cf52a89e83f5295678
GreenCame <julien.mustiere@ntymail.com>
2016-11-06 18:28:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# lightlab
+canvas game multi
+
+<h1> Todos </h1>
+<ul>
+    <li>Cahier des charges</li>
+    <li>Objets</li>
+    <ul>
+        <li>Joueur</li>
+        <li>Labyrinth</li>
+        <li>Timer objet/constante</li>
+        <li>Manager</li>
+    </ul>
+    <li>Connection socket.io</li>
+    <li>Ajout de nodejs</li>
+</ul>
---
+++ app/assets/js/main.js
@@ -0,0 +1,32 @@
+
+//canvas
+var xCanvas = 600;
+var yCanvas = 600;
+
+function setup(){
+    createCanvas(xCanvas,yCanvas);
+}
+
+
+function draw(){
+    background(131,116,103);
+    //TODO
+}
+
+/*function keyPressed(){
+    if (keyCode==68){//lettre D
+        //TODO
+    }
+}
+
+function keyReleased(){
+    //TODO
+}*/
+
+
+
+
+
+
+
+
