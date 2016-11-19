ebd61fe87e209da8c8cdd54cabb58f50513234ef
GreenCame <julien.mustiere@ntymail.com>
2016-11-19 08:49:20 +0100

    main.js changed

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,12 +1,22 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+    //nos fichiers
 }
 
 function create() {
-    var lab = new labyrinth(500, 500, 20);
+    //var timer = Timer(x, y, color, size)
+    var lab = new labyrinth(500, 500, 20);//(x, y, case)
     lab.draw();
+    //STEP 1
+        //var joueur = Joueur(name, x, y, key1, key2, key3, key4, size, color)
+        //var joueur2 = Joueur(name, x, y, key1, key2, key3, key4, size, color)
+    //STEP  2
+        //var joueur = Joueur(name, x, y, key1, key2, key3, key4, size, color, ombre)
 }
 
 function update() {
+    //timer.restart();
+    //timer.draw()
+    //joueur.draw();
 }
